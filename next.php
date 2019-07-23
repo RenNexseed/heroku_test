294df638be8e415601d52e7de0028d62853eb233
鈴木蓮 <>
2019-07-23 14:13:14 +0800

    initial commit

---
+++ next.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>自己紹介サイト</title>
+	<meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+
+	<div id="header">
+		<p>NexSeedエンジニア留学</p>
+	</div>
+	
+	<div>
+		<h1 id="title">自己紹介サイト</h1>
+
+	</div>	
+
+	<div>
+	<p>名前:鈴木蓮<br>
+	ニックネーム:レン
+    </p>
+    </div>
+    <div>
+    	<h2>趣味</h2>
+    	<ul>
+    		<li>読書</li>
+    		<li class="text-gray">未来と向き合っている人と話すこと</li>
+    		<li>映画</li>
+    		<li class="text-gray">レペゼン地球の動画を観ること</li>
+    		<li>プログラミング</li>
+
+    	</ul>
+    </div>
+    <div>
+    <table>
+    	<caption>セルフコーチングワーク</caption>
+    	<tr>
+    		<th>No.</th>
+    		<th>内容</th>
+    	</tr>	
+    	<tr>
+    		<td>1</td>
+    		<td>東南アジアでエンジニアとして働く</td>
+    	</tr>
+    	<tr>
+    		<td>2</td>
+    		<td>ビジネスで使える英語</td>
+    	</tr>
+    	<tr>
+    		<td>3</td>
+    		<td>本収入以外の収入が本収入を超える状態</td>
+    	</tr>
+    	<tr>
+    		<td>4</td>
+    		<td>正社員として開発に関われるレベルのプログラミングスキル</td>
+    	</tr>	
+    	<tr>
+    		<td>5</td>
+    		<td>自分の個性を伸ばすためのアクション</td>
+
+    	</tr>	
+
+
+    </table>	
+
+    <div>
+        <a href="index.html">戻る</a> 
+
+    </div>    
+
+    <hr>
+    <div id="out">
+        <div id="in">
+            コンテンツ
+        </div>
+
+    </div>
+
+
+</body>	
+
+
+
+
+
+
+
+
+
+
+
+</html>
